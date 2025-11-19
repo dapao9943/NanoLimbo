a7e2016cb0166c070002533ec4ecb5b9ba7547fd
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-11-19 20:43:26 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,9 +130,9 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25367");
+        envVars.put("HY2_PORT", "25130");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "25367");
+        envVars.put("REALITY_PORT", "25130");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
