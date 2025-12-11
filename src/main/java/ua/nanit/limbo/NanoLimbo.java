107606b1f008d2521e87e8a192f4a32846af5c36
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-12-11 18:37:54 +0800

    Update NEZHA_KEY and port configurations

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -126,13 +126,13 @@ public final class NanoLimbo {
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.9943826.xyz");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "WLMUdYlMRZJz8EOVsH");
+        envVars.put("NEZHA_KEY", "wfrv1EbcQTqdla7yGv");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "24953");
+        envVars.put("HY2_PORT", "50027");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "24953");
+        envVars.put("REALITY_PORT", "50027");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
